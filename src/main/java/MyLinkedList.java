5af224a933cced231803e85509d92142d67b0678
SergeyBichan <ugidancer@gmail.com>
2024-06-24 14:00:44 +0300

    init

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,48 +1,6 @@
-public class MyLinkedList<E> {
-    private Node<E> firstNode;
-    private Node<E> lastNode;
-    private int size = 0;
-
-    public MyLinkedList() {
-        lastNode = new Node<E>(null, firstNode, null);
-        firstNode = new Node<E>(null, null, lastNode);
-    }
-
-    private class Node<E> {
-        private E currentElement;
-        private Node<E> prevElement;
-        private Node<E> nextElement;
-
-        public Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
-            this.currentElement = currentElement;
-            this.prevElement = prevElement;
-            this.nextElement = nextElement;
-        }
-
-        public E getCurrentElement() {
-            return currentElement;
-        }
-
-        public void setCurrentElement(E currentElement) {
-            this.currentElement = currentElement;
-        }
-
-        public Node<E> getPrevElement() {
-            return prevElement;
-        }
-
-        public void setPrevElement(Node<E> prevElement) {
-            this.prevElement = prevElement;
-        }
-
-        public Node<E> getNextElement() {
-            return nextElement;
-        }
-
-        public void setNextElement(Node<E> nextElement) {
-            this.nextElement = nextElement;
-        }
-    }
-
-
- }
+public interface MyLinkedList<E>{
+    void addLast(E e);
+    void addFirst(E e);
+    int size();
+    E getElementByIndex(int counter);
+}
---
+++ src/main/java/MyLinkedListListImpl.java
@@ -0,0 +1,72 @@
+public class MyLinkedListListImpl<E> implements MyLinkedList<E> {
+    private Node<E> firstNode;
+    private Node<E> lastNode;
+    private int size = 0;
+
+    public MyLinkedListListImpl() {
+        lastNode = new Node<E>(null, firstNode, null);
+        firstNode = new Node<E>(null, null, lastNode);
+    }
+
+    @Override
+    public void addLast(E e) {
+        Node<E> prev = lastNode;
+        prev.setCurrentElement(e);
+        lastNode = new Node<E>(null, prev, null);
+        prev.setNextElement(lastNode);
+        size++;
+    }
+
+    @Override
+    public void addFirst(E e) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public E getElementByIndex(int counter) {
+        return null;
+    }
+
+    private class Node<E> {
+        private E currentElement;
+        private Node<E> prevElement;
+        private Node<E> nextElement;
+
+        public Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
+            this.currentElement = currentElement;
+            this.prevElement = prevElement;
+            this.nextElement = nextElement;
+        }
+
+        public E getCurrentElement() {
+            return currentElement;
+        }
+
+        public void setCurrentElement(E currentElement) {
+            this.currentElement = currentElement;
+        }
+
+        public Node<E> getPrevElement() {
+            return prevElement;
+        }
+
+        public void setPrevElement(Node<E> prevElement) {
+            this.prevElement = prevElement;
+        }
+
+        public Node<E> getNextElement() {
+            return nextElement;
+        }
+
+        public void setNextElement(Node<E> nextElement) {
+            this.nextElement = nextElement;
+        }
+    }
+
+
+ }
